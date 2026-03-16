61411bf4aa231171d56b83046aa8d2a81a9f024a
Olinkoo <olikbielik147@gmail.com>
2026-03-16 14:53:38 +0100

    pridanie footeru a headeru

---
+++ thankyou.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/banner.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <main>
+      <section class="banner">
+        <div class="container text-white">
+          <h1>Ďakujeme</h1>
+        </div>
+      </section>
+      <section class="container">
+        <div class="row">
+          <div class="col-100 text-center">
+              <h2>Ďakujeme za vyplnenie formulára</h2>
+          </div>
+        </div>
+      </section>
+
+
+    </main>
+    <script src="js/menu.js"></script>
+</body>
+<?php  $file_path = "parts/header.php"; if(!include($file_path)) {     echo"Failed to include $file_path";} ?>
+ <?php  $file_path = "parts/footer.php"; if(!include($file_path)) {     echo"Failed to include $file_path";} ?>
+</html>
