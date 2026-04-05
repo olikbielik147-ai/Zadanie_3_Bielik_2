ffdbd45aaa1fa08c5759cd33d2578a7f46815b81
Olinkoo <olikbielik147@gmail.com>
2026-04-05 18:31:19 +0200

    bannery

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php 
+include "function.php"; 
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,60 +12,55 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-    <main>
-      <section class="slides-container">
-        <div class="slide fade">
-          <img src="img/banner1.jpg">
-          <div class="slide-text">
-            Prvý nadpis
-          </div>
-        </div>
-        
-        <div class="slide fade">
-          <img src="img/banner2.jpg">
-          <div class="slide-text">
-            Druhý nadpis
-          </div>
-        </div>
-        
-        <div class="slide fade">
-          <img src="img/banner3.jpg">
-          <div class="slide-text">
-            Tretí nadpis
-          </div>
-        </div>
-        
+
+<?php 
+$file_path = "parts/header.php"; 
+if (!include($file_path)) { 
+    echo "Failed to include $file_path";
+}
+?>
+
+<main>
+
+    <section class="slides-container">
+        <?php render_banners(); ?>
         <a id="prev" class="prev">❮</a>
         <a id="next" class="next">❯</a>
-        
-      </section>
-      <section class="container">
-        <div class="row">
-          <div class="col-100 text-center">
-              <p><strong><em>Elit culpa id mollit irure sit. Ex ut et ea esse culpa officia ea incididunt elit velit veniam qui. Mollit deserunt culpa incididunt laborum commodo in culpa.</em></strong></p>
-          </div>
-        </div>
-      </section>
-      <section class="container">
-        <div class="row">
-          <div class="col-50">
-            <h2>Mollit cupidatat velit quis irure non eiusmod culpa cillum velit magna est aliquip.</h2>
-          </div>
-          <div class="col-50">
-            <p>Nulla dolore sit esse pariatur culpa sint nulla fugiat nulla ut. Sit adipisicing fugiat id consequat qui est. Nulla ea aliquip culpa nulla mollit do excepteur ex ullamco consequat tempor consectetur. Tempor et ea voluptate irure est magna magna reprehenderit qui non dolore. Consequat id ad cillum do qui. Proident ipsum enim eiusmod fugiat quis anim consequat ullamco mollit excepteur. Eiusmod eu ad irure anim velit laborum commodo nostrud pariatur id ea ad.</p>
-            <p>Incididunt pariatur ipsum in culpa sit cillum consequat nostrud do. Esse commodo ad qui ea. Pariatur elit mollit deserunt aute do culpa eu veniam pariatur in adipisicing qui adipisicing irure. Est minim sint ipsum nulla eiusmod. Amet do officia consectetur labore. Id aute labore culpa aliquip. Adipisicing mollit excepteur Lorem quis elit ipsum irure quis occaecat qui consectetur ad irure dolor.</p>
-          </div>
-        </div>
-      </section>
-    </main>
+    </section>
 
-    <script src="js/menu.js"></script>
-    <script src="js/slider.js"></script>
-</body>
-<?php  $file_path = "parts/header.php"; if(!include($file_path)) {     echo"Failed to include $file_path";} ?>
- <?php  $file_path = "parts/footer.php"; if(!include($file_path)) {     echo"Failed to include $file_path";} ?>
- <?php
-require 'function.php';
-$data = getData();
+    <section class="container">
+        <div class="row">
+            <div class="col-100 text-center">
+                <p><strong><em>
+                    Elit culpa id mollit irure sit. Ex ut et ea esse culpa officia ea incididunt elit velit veniam qui.
+                </em></strong></p>
+            </div>
+        </div>
+    </section>
+
+    <section class="container">
+        <div class="row">
+            <div class="col-50">
+                <h2>Mollit cupidatat velit quis irure non eiusmod culpa cillum velit magna est aliquip.</h2>
+            </div>
+            <div class="col-50">
+                <p>Nulla dolore sit esse pariatur culpa sint nulla fugiat nulla ut...</p>
+                <p>Incididunt pariatur ipsum in culpa sit cillum consequat nostrud do...</p>
+            </div>
+        </div>
+    </section>
+
+</main>
+
+<?php 
+$file_path = "parts/footer.php"; 
+if (!include($file_path)) { 
+    echo "Failed to include $file_path";
+}
 ?>
+
+<script src="js/menu.js"></script>
+<script src="js/slider.js"></script>
+
+</body>
 </html>
