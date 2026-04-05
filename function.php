97aa98330c6ef5604cba234b913437d9eb277a00
Olinkoo <olikbielik147@gmail.com>
2026-04-05 18:13:00 +0200

    pridanie json funkcie

--- function.php
+++ function.php
@@ -21,3 +21,30 @@ function generateSlides($dir) {
     }
 }
 ?>
+
+<?php
+
+function load_banner_data() {
+    $path = "data/banner.json";
+    if (!file_exists($path)) {
+        return null;
+    }
+
+    $json = file_get_contents($path);
+    return json_decode($json, true);
+}
+
+function render_banners() {
+    $data = load_banner_data();
+    if (!$data) return;
+
+    foreach ($data["text_banner"] as $img => $info) {
+        echo '
+        <div class="slide fade" onclick="window.location.href=\'' . $info["url"] . '\'">
+            <img src="img/banners/' . $img . '">
+            <div class="slide-text">' . $info["text"] . '</div>
+        </div>';
+    }
+}
+
+?>
